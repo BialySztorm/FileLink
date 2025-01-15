3fb3df7cf1883bb30613c8a1e59f7361dbaccb92
WhiteStorm <andrzejmmm1@gmail.com>
2025-01-15 12:06:18 +0100

    Add websocket buffer handling
    Implemented handling of file metadata and data buffers in WebSocket server. Metadata is stored and file data is accumulated until EOF signal, then saved to database.

--- src/Service/WebSocketServer.php
+++ src/Service/WebSocketServer.php
@@ -8,30 +8,61 @@ use Ratchet\ConnectionInterface;
 class WebSocketServer implements MessageComponentInterface
 {
     protected $clients;
+    protected $fileData;
+
     public function __construct()
     {
         $this->clients = new \SplObjectStorage();
-        // Initialize the server
+        $this->fileData = new \SplObjectStorage();
     }
+
     public function onOpen(ConnectionInterface $conn)
     {
-        // Store the new connection
         $this->clients->attach($conn);
-        $conn->send('Welcome to the WebSocket server');
     }
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
-        // Handle incoming message
+        $data = json_decode($msg, true);
+        if (isset($data['fileMetadata'])) {
+            // Handle metadata
+//            error_log($msg);
+            $this->fileData[$from] = [
+                'metadata' => $data,
+                'buffer' => ''
+            ];
+            $from->send('{"message":"Metadata received"}');
+        } else {
+            // Handle file data
+            if (isset($this->fileData[$from])) {
+                $fileInfo = $this->fileData[$from];
+                if ($msg === "\0") { // EOF signal
+                    // Save the file data
+//                    error_log('Saving ' . strlen($fileInfo['buffer']) . ' bytes');
+                    // TODO Save to database
+                    $from->send('{"message":"File data received"}');
+                    unset($this->fileData[$from]);
+                } else {
+                    // Append data to buffer
+//                    error_log('Received ' . strlen($msg) . ' bytes');
+                    $fileInfo['buffer'] .= $msg;
+                    $this->fileData[$from] = $fileInfo;
+                }
+            } else {
+                error_log('Metadata not received');
+                $from->send('{"Error":"Metadata not received"}');
+            }
+        }
     }
 
     public function onClose(ConnectionInterface $conn)
     {
-        // Handle connection close
+        $this->clients->detach($conn);
+        unset($this->fileData[$conn]);
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e)
     {
-        // Handle error
+        $conn->close();
     }
 }
